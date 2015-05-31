6c919edf7f9ca089925cd9c1ab76e3fad12f01fd
Gregory <greg.kitzmiller@gmail.com>
2015-05-31 13:03:23 -0700

    green #valid fileReader object

--- src/Lab1Tests.java
+++ src/Lab1Tests.java
@@ -24,13 +24,29 @@ public class Lab1Tests {
     @Test
     public void constructor_fails_gracefully_on_null_file(){
         //arrange/act
-        Lab1 l = new Lab1("inputData.txt");
+        Lab1 l = new Lab1("inputFileInvalid.txt");
 
         //assert
             assertNull(l.fileReader); //file is null
 
         //since it's null, make sure it outputs a message
             assertEquals("File not found.", outContent.toString());
+    }
+
+
+    //test file that actually exists and reads into an array
+    @Test
+    public void test_constructs_and_reads_to_array(){
+        //arrange/act
+        String fileString = "inputFile.txt";
+        Lab1 l = new Lab1(fileString);
+
+        //assert
+        assertEquals(l.fileName, fileString); //passed in properly
+        assertNotNull(l.fileReader);
+
+
+
     }
 
 }
